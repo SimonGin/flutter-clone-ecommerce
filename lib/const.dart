4040d7897c21c44299caf1ef4ec9a9cc647f3ca1
Simon Gin <simongin@192.168.2.9>
2023-09-14 15:23:46 +0700

    Add const.dart for constant variables to use in the project

---
+++ lib/const.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const k1stColor = Color(0xFFFF7643);
+const k1stLightColor = Color(0xFFFFECDF);
+const k1stGradColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+);
+const k2ndColor = Color(0xFF979797);
+const kTextColor = Color(0xFF757575);
+
+const kAnimDuration = Duration(milliseconds: 200);
